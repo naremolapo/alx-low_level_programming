c903fdf730565f40bea2c9e53a567d3abc6201ec
nare molapo <molaponare@gmail.com>
2023-07-19 16:36:02 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,9 @@
  * Return: if character is lowercase, otherwise
  */
 
-int _islower(int C)
+int _islower(int c)
 {
-	if (C  >= 'a' && C <= 'z')
+	if (c  >= 'a' && c <= 'z')
 		return(1);
 	else
 		return(0);

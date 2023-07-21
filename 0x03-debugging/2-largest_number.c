e7f48862c8296d3dd1017eae0a2bb6b88978988b
nare molapo <molaponare@gmail.com>
2023-07-20 18:38:02 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,18 +16,16 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (a > b && c > a)
+    else if (b > c )
     {
-        largest = c;
-    }
-    else if (b > c)
-    {
-	    largest = b;
+        largest = b;
     }
     else
     {
         largest = c;
     }
 
-return (largest);
+    return (largest);
 }
+
+

793fe1695dffda573304887fa7ebabe2c2724e47
nare molapo <molaponare@gmail.com>
2023-07-21 13:34:00 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
 * _isdigit - check if a character is a digit
-* @x: The number to be checked 
+* @x: The number to be checked
 * Return: 1 for a character that will be a digit or 0 for any else
 */
 
